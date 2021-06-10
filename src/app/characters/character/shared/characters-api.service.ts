6532858df734df8b10ed8a1c198120e24757ee78
Eduardo <54048170+Eduardo-ops@users.noreply.github.com>
2021-06-09 21:10:00 -0300

    Adicionado comentarios

--- src/app/characters/character/shared/characters-api.service.ts
+++ src/app/characters/character/shared/characters-api.service.ts
@@ -8,8 +8,19 @@ import { map } from 'rxjs/operators';
 })
 export class CharactersApiService {
 
+  /* 
+  Chave pública adquirida no site da marvel
+   */
   PUBLIC_KEY = '56dfe9c13876bad301ae2d7843fc8885'
+
+  /* 
+  Hash gerado por um site com a combinação de 1 + chave privada + chave puclica 
+  */
   HASH = '86b8e6d9c8afb9d2ceecfd7df01db315'
+
+  /* 
+  Endereço dos dados que escolhemos obter da marvel 
+  */
   URL_API = `http://gateway.marvel.com/v1/public/characters?ts=1&apikey=${this.PUBLIC_KEY}&hash=${this.HASH}`
 
   constructor(private http: HttpClient) { }
