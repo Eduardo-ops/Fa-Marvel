d0311a7ec6991572fe519e4a9cb08bba4c3447b8
Eduardo <54048170+Eduardo-ops@users.noreply.github.com>
2021-06-22 20:28:52 -0300

    Removendo linhas de codigo comentadas e sem utilidade.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,11 +7,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  /*  {
-     path: '',
-     redirectTo: '/characters',
-     pathMatch: 'full'
-   }, */
   {
     path: 'characters',
     component: CharactersComponent
